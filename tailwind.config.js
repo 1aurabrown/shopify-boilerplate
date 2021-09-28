053db44d53f78e9306f27f5a51f3d5d0b74d73c9
Laura Brown <laura@laurabrown.xyz>
2021-09-28 12:26:06 +0200

    update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,17 +28,21 @@ module.exports = {
       '85': '.85',
       '100': '1',
     },
-    inset: tailwindSpacing,
     transitionProperty: {
       'max-h': 'max-height',
       'opacity': 'opacity',
       'all': 'all',
     },
-    gap: tailwindSpacing,
-    spacing: tailwindSpacing,
     extend: {
+      inset: tailwindSpacing,
+      gap: tailwindSpacing,
+      spacing: tailwindSpacing,
+      // If you change the breakpoints, don't forget to update breakpoints-tailwind.js
       screens: {
         'hoverable': {'raw': '(hover: hover)'},
+        'xs-down': {'max' : '639px'},
+        'sm-down': {'max': '767px'},
+        'md-down': {'max': '1023px'}
       },
       maxHeight: {
         '0': '0'
@@ -48,7 +52,10 @@ module.exports = {
       }
     }
   },
-  corePlugins: {
-    textTransform: false,
-  }
+  variants: {
+    extend: {
+      // ...
+     fontFamily: ['hover', 'focus'],
+    }
+  },
 }
